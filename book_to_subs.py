2c4b4836c8a885008456bc69ac03340075a48ddf
AtilioA <atiliodadalto@hotmail.com>
2020-08-14 15:56:40 -0300

    fix: remove hardcoded paths
    tweak task_adjust_boundary_nonspeech_min

--- book_to_subs.py
+++ book_to_subs.py
@@ -7,6 +7,10 @@ import subprocess
 
 import nltk
 
+AUDIO_DIR = "audio"
+TXT_DIR = "txt"
+SRT_DIR = "srt"
+
 
 # Parse cli arguments
 parser = argparse.ArgumentParser(description="Segment a .txt file.")
@@ -24,16 +28,18 @@ inputFilepathStem = Path(inputFilepath).stem
 
 # Segment input file
 sentenceTokenizer = nltk.data.load(f"tokenizers/punkt/{language}.pickle")
-rawText = Path(f"txt/{inputFilepath}").read_text()
+rawText = Path(f"{TXT_DIR}/{inputFilepath}").read_text()
 segmentedSentences = sentenceTokenizer.tokenize(rawText)
 print(f"Segmented '{inputFilepath}' into {len(segmentedSentences)} sentences.")
 
 # Write segmented sentences to output file
-with open(f"txt/{inputFilepathStem}_seg.txt", "w+", encoding="utf-8",) as segmentedTxt:
+with open(
+    f"{TXT_DIR}/{inputFilepathStem}_seg.txt", "w+", encoding="utf-8",
+) as segmentedTxt:
     for sentence in segmentedSentences:
         segmentedTxt.write(f"{sentence.strip()}\n")
 
 # Call aeneas with audio and output file (segmented input file) and write to .srt file
-bashCommand = f'/usr/bin/python3 -m aeneas.tools.execute_task "audio/{inputFilepath[:-7]}.mp3" "txt/{inputFilepathStem}_seg.txt" "task_language={languageCode}|is_text_type=plain|os_task_file_format=srt|task_adjust_boundary_nonspeech_min=0.350|task_adjust_boundary_nonspeech_string=REMOVE" "{inputFilepathStem}.srt"'
+bashCommand = f'/usr/bin/python3 -m aeneas.tools.execute_task "{AUDIO_DIR}/{inputFilepath[:-7]}.mp3" "{TXT_DIR}/{inputFilepathStem}_seg.txt" "task_language={languageCode}|is_text_type=plain|os_task_file_format=srt|task_adjust_boundary_nonspeech_min=0.500|task_adjust_boundary_nonspeech_string=REMOVE" "{SRT_DIR}/{inputFilepathStem}.srt"'
 # print(bashCommand)
 process = subprocess.run(bashCommand, shell=True)
